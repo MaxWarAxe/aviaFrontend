873ee0bead51cfc9c5d79870869ac247f715592f
maxwaraxe <makskriper9@gmail.com>
2024-06-01 13:32:11 +0300

    cards

---
+++ src/components/PlaceCard.vue
@@ -0,0 +1,23 @@
+
+<script setup>
+
+</script>
+
+<template>
+    <div class="card-size relative rounded-3xl bg-blue-950 inline-block">
+        <div class="text-white text-2xl relative top-4 left-4">ЖАRA</div>
+        <div>
+            <v-icon class="relative bottom-0 left-4" icon="mdi-star" color="yellow"></v-icon>
+        </div>
+    </div>
+</template>
+
+
+<style  scoped>
+    .card-size{
+        width:  300px;
+        height: 200px;
+        margin-right: 25px;
+        margin-bottom: 25px;
+    }
+</style>
--- src/views/MainTripPage.vue
+++ src/views/MainTripPage.vue
@@ -1,4 +1,20 @@
 <script setup>
+    import {ref} from 'vue'
+    import TripCard from '@/components/TripCard.vue'
+    import PlaceCard from '@/components/PlaceCard.vue'
+
+    const placeFrom = ref()
+    const placeTo = ref()
+    function swapPlaces(){
+        const placeTmp = placeFrom.value
+        placeFrom.value = placeTo.value
+        placeTo.value = placeTmp
+    }
+
+    function clearPlaces(){
+      placeTo.value = ''
+      placeFrom.value = ''
+    }
 
 </script>
 
@@ -8,34 +24,47 @@
             <img src="@/assets/logos/Zephyr.svg" class="w-24 inline" alt="">
             <ul class="flex float-right">
                 <li>Конструктор</li>
-                <li>Войти</li>
-                <li>Зарегистрироваться</li>
+                <li><router-link to="/login">Войти</router-link> </li>
+                <li><router-link to="/registration">Зарегистрироваться</router-link></li>
             </ul>
         </header>
         <main class="flex justify-center">
-            <div class="main-container p-8 ">
+            <div class="main-container pt-8 ">
                 <div class="text-5xl font-bold mb-10">ВАШЕ ПУТЕШЕСТВИЕ</div>
 
                 <div class="flex align-middle items-center content-center">
                     <p class=" font-bold text-3xl mr-5">ПУНКТ НАЗНАЧЕНИЯ</p>
                     <div class="line bg-black mr-5"></div>
-                    <p class=" text-blue-700">СБРОСИТЬ</p>
+                    <p @click="clearPlaces" class="cursor-pointer text-blue-700">СБРОСИТЬ</p>
                 </div>
-                <p class="text-2xl">Уверяю все будет хорошо!</p>
+                <p class="mb-6 text-2xl">Уверяю все будет хорошо!</p>
                
-                <div class="flex">
-                    <v-text-field label="Начальная точка" class="h-14" variant="underlined" prepend-inner-icon="mdi-magnify"></v-text-field>
-                    <div>
-                        <v-icon size="x-large" icon="mdi-magnify"></v-icon>
+                <div class="flex mb-5">
+                    <v-text-field v-model="placeFrom" class="mr-20" label="Начальная точка" variant="underlined" prepend-inner-icon="mdi-magnify"></v-text-field>
+                    <div class="anim cursor-pointer" @click="swapPlaces">
+                        <v-icon class="mr-20 mt-5" size="x-large" icon="mdi-swap-horizontal"></v-icon>
                     </div>
-                    <v-text-field label="Конечная точка" class="h-14" variant="underlined" prepend-inner-icon="mdi-magnify"></v-text-field>
+                    <v-text-field v-model="placeTo" label="Конечная точка" class="h-14" variant="underlined" prepend-inner-icon="mdi-magnify"></v-text-field>
                 </div>
-            </div>
+                <TripCard class="mb-5 ml-" v-for="i in 2"></TripCard>
+
+                <div class="mt-10 flex align-middle items-center content-center">
+                    <p class=" font-bold text-3xl mr-5">ПОПУЛЯРНЫЕ МЕСТА</p>
+                    <div class="line bg-black mr-5"></div>
+                </div>
+                <p class="mb-6 text-2xl">Вам может понравиться!</p>
+
+                <div class="grid gap-6 grid-cols-3 grid-flow-row">
+                  <PlaceCard v-for="i in 20"></PlaceCard>
+                </div>
+
+              </div>
         </main>
     </div>
-</template>
+</template>963
 
 <style>
+
   li{
     color: white;
     text-transform: uppercase;
@@ -43,9 +72,9 @@
   }
   .line{
     height: 2px;
-    width: 400px;
+    width: auto;
   }
   .main-container{
-    width:963px;
+    width: 963px;
   }
 </style>
